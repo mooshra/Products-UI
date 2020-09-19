04a434c425817827a215259f3e7ce6af02de46f6
Dmitry Kochegura <31731931+dkochegura@users.noreply.github.com>
2020-09-19 19:08:51 +0300

    Layout & Styles Refactoring

--- src/app/products/models/product.ts
+++ src/app/products/models/product.ts
@@ -18,4 +18,52 @@ export const initialProducts: Product[] = [
     price: 111,
     category: 'category',
   },
+  {
+    productId: 3,
+    name: 'test 3',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 4,
+    name: 'test 4',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 5,
+    name: 'test 5',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 6,
+    name: 'test 6',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 7,
+    name: 'test 7',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 8,
+    name: 'test 8',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 9,
+    name: 'test 9',
+    price: 111,
+    category: 'category',
+  },
+  {
+    productId: 10,
+    name: 'test 10',
+    price: 111,
+    category: 'category',
+  },
 ];
